2cbeb3117a11e0755b0282c7b16afd712833053d
Andrey <Rewsr-Edu@yandex.ru>
2022-03-16 23:11:45 +0300

    ex1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// Задача 1
 
+Console.WriteLine("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine ("Первое число больше");
+}
+else
+{
+    Console.WriteLine ("Второе число больше");
+}
